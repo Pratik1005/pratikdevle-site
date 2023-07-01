89d8227e93da7c652473a8bbcb32d8d65562401a
Pratik Devle <pratik.devle98@gmail.com>
2023-07-01 21:16:07 +0530

    feat: added project section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,13 @@
-import {NavLink} from "react-router-dom";
 import {NavMenu} from "../components/NavMenu";
+import {HeroText} from "../components/HeroText";
+import {Project} from "../components/Project";
 
 export const Home = () => {
   return (
     <div>
       <NavMenu />
-      <section className="hero-text">
-        <h1>Hi, I'm Pratik</h1>
-        <h2>A Frontend Engineer</h2>
-        <p>
-          I'm passionate about building user interfaces that are engaging,
-          accessible and user-centric.
-        </p>
-      </section>
-
-      <section className="projects">
-        <h2 className="section-title">Projects</h2>
-      </section>
+      <HeroText />
+      <Project />
       <section>
         <h2 className="section-title">Blogs</h2>
       </section>
